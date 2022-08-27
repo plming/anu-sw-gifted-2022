60aa1d7bfedc81d7bd4cef0a62e18035227c6f63
Kim Hyeonseok <kim010322@gmail.com>
2022-08-27 12:28:00 +0900

    add distance.py

--- common.py
+++ common.py
@@ -1,3 +1,5 @@
+
+
 def load_polar_scores():
     polar_scores = {}
     file = open('dicty.txt', 'r', encoding='utf-8')
@@ -9,19 +11,103 @@ def load_polar_scores():
     return polar_scores
 
 
-
 def get_word_count_pair_list(word_list: list):
     '''
     명사 리스트를 (명사, 빈도수) 리스트로 변환
     get_word_count_pair_list(['apple', 'apple']) == [('apple', 2)] 
     '''
     word_list = sorted(word_list)
-    result = []
+    result = {}
     start = 0
     end = 0
     while start < len(word_list):
         while end < len(word_list) and word_list[start] == word_list[end]:
             end = end + 1
-        result.append((word_list[start], end - start))
+        result[word_list[start]] = end - start
         start = end
     return result
+
+
+def combi2(word):
+    n = len(word)
+    for i in range(n-1, 0, -1):
+        if (word[0:i] in nouns and word[i:n] in haday):
+            return [word[0:i]]
+        elif (word[0:i] in nouns and word[i:n] in nouns):
+            return [word[0:i], word[i:n]]
+        elif (word[0:i] in nouns and word[i:n] in postpositions):
+            return [word[0:i]]
+    return []
+
+
+def combi3(word):
+    n = len(word)
+    for i in range(1, n):
+        for j in range(i+1, n):
+            if (word[0:i] in nouns and word[i:j] in nouns and word[j:n] in postpositions):
+                return [word[0:i], word[i:j]]
+    return []
+
+
+def get_nouns():
+    nounf = open('noun.txt', 'r', encoding='utf-8')
+    nounl = nounf.readlines()
+    noun = []
+    for i in range(len(nounl)):
+        noun.append(nounl[i].strip('.\n'))
+    return noun
+
+
+def get_postpositions():
+    postpf = open('postPosition.txt', 'r', encoding='utf-8')
+    postpl = postpf.readlines()
+    postpositions = []
+    for i in range(len(postpl)):
+        postpositions.append(postpl[i].strip('.\n'))
+    return postpositions
+
+
+def get_haday():
+    hadayf = open('joyolist.txt', 'r', encoding='utf-8')
+    hadayl = hadayf.readlines()
+    haday = []
+    for i in range(len(hadayl)):
+        haday.append(hadayl[i].strip('.\n'))
+    return haday
+
+
+def get_comments() -> list[dict]:
+    result = []
+
+    nouns = get_nouns()
+
+    file = open('news_list.csv', 'r', encoding='utf-8')
+    comments = file.readlines()
+    for comment in comments:
+        word_list = comment.split(' ')
+
+        # 댓글에서 명사 분리
+        noun_list = []
+        for i in range(len(word_list)):
+            if word_list[i] in nouns:
+                noun_list.append(word_list[i])
+            elif combi2(word_list[i]) != []:
+                t = combi2(word_list[i])
+                for j in range(len(t)):
+                    noun_list.append(t[j])
+            elif combi3(word_list[i]) != []:
+                t = combi3(word_list[i])
+                for j in range(len(t)):
+                    noun_list.append(t[j])
+
+        # 댓글을 {단어: 빈도수}로 만들기
+        parsed = get_word_count_pair_list(noun_list)
+        result.append(parsed)
+
+    return result
+
+
+# global variables
+nouns = get_nouns()
+haday = get_haday()
+postpositions = get_postpositions()
---
+++ distance.py
@@ -0,0 +1,52 @@
+"""
+1. 명사 분리
+
+2. 모든 댓글에 대해 max, min 구하기
+3. 댓글 2개의 거리 구하기 - 최대, 최소
+
+"""
+from math import sqrt
+from common import *
+
+
+def get_max_min(comments: list[dict]):
+    """
+    { "apple": {
+        "max": 1,
+        "min": 2
+    }
+    }
+    """
+    result = {}
+    for comment in comments:
+        for word, count in comment.items():
+            if word not in result:
+                result[word] = {"max": count, "min": count}
+            else:
+                if count > result[word]["max"]:
+                    result[word]["max"] = count
+
+                if count < result[word]["min"]:
+                    result[word]["min"] = count
+    return result
+
+
+def get_distance(first, second, max_min) -> float:
+    sum_term = 0
+    for word, value in max_min.items():
+        dividend = (first.get(word, 0) - second.get(word, 0))
+        divisor = value['max'] - value['min']
+        divisor = 1 if divisor == 0 else divisor
+        sum_term += (dividend/divisor) ** 2
+
+    return sqrt(sum_term)
+
+
+comments = get_comments()
+
+max_min = get_max_min(comments)
+
+for i in range(len(comments)):
+    for j in range(i+1, len(comments)):
+        distance = get_distance(comments[i], comments[j], max_min)
+        print(f"dist between {i} and {j}: {distance}")
