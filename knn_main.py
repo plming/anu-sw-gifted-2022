4672f4dd6e271cd87a96a8d8ef9080bc29b7b7e6
Kim Hyeonseok <kim010322@gmail.com>
2022-09-12 17:16:08 +0900

    add main entry point

--- knn_main.py
+++ knn_main.py
@@ -3,14 +3,15 @@ from knn import KNN
 from label import Label
 from time import time
 
-TEST_TEXT = '정말 좋습니다'
+if __name__ == '__main__' :
+    TEST_TEXT = '정말 좋습니다'
 
-start = time()
-train_data = read_from_csv('news.csv')
-test_data = Comment(TEST_TEXT, Label.UNKNOWN)
+    start = time()
+    train_data = read_from_csv('news.csv')
+    test_data = Comment(TEST_TEXT, Label.UNKNOWN)
 
-model = KNN(train_data)
-is_positive = model.predict(test_data)
-print(f'예측 결과: {is_positive}')
+    model = KNN(train_data)
+    is_positive = model.predict(test_data)
+    print(f'예측 결과: {is_positive}')
 
-print(f'수행시간(s): {time() - start}')
+    print(f'수행시간(s): {time() - start}')
